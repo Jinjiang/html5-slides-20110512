30654b19ef8c12e42d4024f5db736c75f2c83ce6
Jinjiang Zhao <zhaojinjiang@yahoo.com.cn>
2011-05-13 18:10:08 +0800

    completed more comments

--- js/script.ie.js
+++ js/script.ie.js
@@ -7,15 +7,20 @@
 
 
 
+// 舍弃页面内动画的效果
 addHook(hooksBeforeInit, function () {
     $('body > div').addClass('slide');
     $('.actable').removeClass('actable');
 });
 
+
+// 初始化显示默认的幻灯片
 addHook(hooksAfterInit, function () {
     $('#slide' + currentPage).addClass('current-slide');
 });
 
+
+// 由于不支持:target伪类，故通过这种方式进行翻页
 addHook(hooksAfterNav, function () {
     $('.current-slide').removeClass('current-slide');
     $('#slide' + currentPage).addClass('current-slide');
